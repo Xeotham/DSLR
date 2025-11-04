4d66939a91d1bbe85de49e0c3ad4cbd964f77d7f
mhaouas <mhaouas@student.42angouleme.fr>
2025-11-04 18:23:00 +0100

    Creating scatter_plot.py to show plot relation as Scatter Plot. Can input the name of the subjects you want to compare to the other.

---
+++ scatter_plot.py
@@ -0,0 +1,116 @@
+#!./.venv/bin/python
+
+from pandas import DataFrame, read_csv
+from numpy import ndarray, array, vstack
+from matplotlib.pyplot import subplots, show, Figure
+from sys import argv
+from dslr_lib.errors import print_error
+from dslr_lib.maths import total_count, count, mean, var, std, min, max, Q1, Q2, Q3, nan_count
+
+error = {
+    "ARG_ERR" : "Error: Invalid number of arguments.",
+    "PARAM_ERR" : "Error: Invalid parameters.",
+    "ALL_ERR": "Error: You can only use the \"All\" parameter alone."
+}
+
+houses_colors = {
+    "Ravenclaw": "c",
+    "Gryffindor": "r",
+    "Slytherin": "g",
+    "Hufflepuff": "y"
+}
+
+def setup_scatter(df: DataFrame) -> dict[str, DataFrame]:
+    """
+    Separate a DataFrame into sub-DataFrames, one for each Hogwarts house, containing only numerical columns.
+
+    Args:
+        df (DataFrame): Input DataFrame containing student data, including a "Hogwarts House" column.
+
+    Returns:
+        dict[str, DataFrame]: A dictionary where keys are Hogwarts house names and values are DataFrames
+                              containing only numerical columns for students of that house.
+    """
+    hogwarts_house = df["Hogwarts House"]
+    hogwarts_house_names = ["Ravenclaw", "Slytherin", "Hufflepuff", "Gryffindor"]
+    return {name: df[hogwarts_house == name].select_dtypes(include="number") for name in hogwarts_house_names}
+
+def show_plot(df: DataFrame, to_show: list) -> None:
+    """
+    Generate and display a grid of scatter plots for all pairs of numerical columns in the DataFrame,
+    with points colored by Hogwarts house.
+
+    Args:
+        to_show:
+        df (DataFrame): Input DataFrame containing student data, including a "Hogwarts House" column.
+
+    Returns:
+        None: Displays the scatter plot grid using matplotlib.
+    """
+
+    if to_show[0] == "All":
+        sorted_df = df.select_dtypes(include="number")
+    else:
+        sorted_df = df.select_dtypes(include="number")[to_show]
+
+    number_df = df.select_dtypes(include="number")
+    plot_dict: dict[str , tuple[Figure, ndarray]] = {
+        name:
+        subplots(
+            4,
+            3
+        )
+        for name in to_show
+    }
+    separated_house = setup_scatter(df)
+
+    for x, plt in zip(plot_dict.keys(), plot_dict.values()):
+        axs_index = 0
+        df_drop_x = number_df.drop(x, axis=1)
+        for i, y in enumerate(df_drop_x):
+            plt[0].suptitle(x)
+            actual_plt = plt[1][i % 4, axs_index]
+            actual_plt.set_title(y)
+            for house, h_df in zip(separated_house.keys(), separated_house.values()):
+                actual_plt.scatter(
+                    h_df[x],
+                    h_df[y],
+                    color=houses_colors[house],
+                    alpha=0.7
+                )
+            if (i + 1) % 4 < i % 4:
+                axs_index += 1
+    show()
+    return
+
+def main() -> None:
+    """
+    Main function to read a CSV file and display scatter plots of the data.
+
+    Args:
+        None: Expects a single command-line argument: the path to the CSV file.
+
+    Returns:
+        None: Displays scatter plots or prints an error message if arguments are invalid.
+    """
+    try:
+        assert len(argv) >= 3, "ARG_ERR"
+
+        df: DataFrame = read_csv(argv[1], header=0).drop("Index", axis=1)
+        subjects_name = df.select_dtypes(include="number").columns.tolist()
+        subjects_name.append("All")
+        for arg in argv[2:]:
+            if arg == "All" and len(argv) != 3:
+                raise AssertionError("ALL_ERR")
+            assert arg in subjects_name, "PARAM_ERR"
+        if "All" in argv[2:]:
+            subjects_list = df.select_dtypes(include="number").columns.tolist()
+        else:
+            subjects_list = argv[2:]
+        show_plot(df, subjects_list)
+    except AssertionError as err:
+        print_error(error[str(err)])
+
+
+if __name__ == "__main__":
+    main()
